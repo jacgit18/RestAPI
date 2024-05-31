169654a44aa42cbd95183eda225de1aad7ddc56c
jacgit18 <joshuaxcarpentier@gmail.com>
2024-05-31 19:59:54 -0400

    edit

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import morgan from "morgan";
 // import path from 'path';
 import * as routes from "./routers/index.ts";
+// import { appUserIdFromHeaders, convertQueryOperators } from "./middlewares/index.ts"
 
 
 const app = express()
@@ -17,6 +18,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // app.use(express.static(path.join(__dirname, "static", "about.html")))
+app.use(appUserIdFromHeaders)
+
+
 
 
 app.use("/v1/api/shows", routes.exampleRouter)
